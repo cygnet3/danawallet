9e82b94cc6241d326ea5a59ed1e484f569499e56
cygnet <cygnet3@proton.me>
2023-11-01 14:55:14 +0100

    Add navigation bar and split wallet stuff into wallet.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: avoid_print
 
-import 'dart:io';
-
+import 'package:donationwallet/settings.dart';
+import 'package:donationwallet/wallet.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:donationwallet/ffi.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,235 +14,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Silent payments',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Silent payments'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int amount = 0;
-  int tipheight = 0;
-  int scanheight = 0;
-  int peercount = 0;
+  int _selectedIndex = 0;
+  final List<Widget> _widgetOptions = [
+    const WalletScreen(),
+    SettingsScreen(),
+  ];
 
-  String spaddress = '';
-
-  double progress = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-    _setup();
-  }
-
-  Future<void> _setup() async {
-    api.createLogStream().listen((event) {
-      print('RUST: ${event.msg}');
-    });
-
-    api.createScanProgressStream().listen(((event) {
-      int start = event.start;
-      int current = event.current;
-      int end = event.end;
-      double scanned = (current - start).toDouble();
-      double total = (end - start).toDouble();
-      double progress = scanned / total;
-      if (current == end) {
-        progress = 0.0;
-      }
-      setState(() {
-        this.progress = progress;
-        scanheight = current;
-      });
-    }));
-
-    api.createAmountStream().listen((event) {
-      setState(() {
-        amount = event;
-      });
-    });
-
-    final Directory appDocumentsDir = await getApplicationSupportDirectory();
-
-    // this sets up everything except nakamoto
-    await api.setup(filesDir: appDocumentsDir.path);
-
-    final amt = await api.getAmount();
+  void _onItemTapped(int index) {
     setState(() {
-      amount = amt;
+      _selectedIndex = index;
     });
-
-    // this starts nakamoto, will block forever
-    api.startNakamoto(filesDir: appDocumentsDir.path);
-  }
-
-  void _updateWalletInfo() async {
-    print('getting wallet info');
-    final info = await api.getWalletInfo();
-    final spaddress = await api.getReceivingAddress();
-
-    setState(() {
-      scanheight = info.scanHeight;
-      tipheight = info.blockTip;
-      amount = info.amount;
-      this.spaddress = spaddress;
-    });
-
-    print('scan height ${info.scanHeight}');
-    print('block height ${info.blockTip}');
-  }
-
-  Future<void> _scanBlocks(int amount) async {
-    await api.scanNextNBlocks(n: amount);
-  }
-
-  Future<void> _resetWallet() async {
-    await api.resetWallet();
-    SystemNavigator.pop();
-  }
-
-  Future<void> _scanToTip() async {
-    await api.scanToTip();
-  }
-
-  void _getPeerCount() async {
-    final peercount = await api.getPeerCount();
-
-    setState(() {
-      this.peercount = peercount;
-    });
-  }
-
-  Widget showScanText() {
-    final toScan = tipheight - scanheight;
-
-    if (scanheight == 0) {
-      return Text('Press \'update peer count\'',
-          style: Theme.of(context).textTheme.headlineSmall);
-    } else if (toScan == 0) {
-      return Text('Up to date!',
-          style: Theme.of(context).textTheme.headlineSmall);
-    } else {
-      return Text(
-        'New blocks: $toScan',
-        style: Theme.of(context).textTheme.headlineSmall,
-      );
-    }
-  }
-
-  Widget getAddress() {
-    double screenWidth = MediaQuery.of(context).size.width;
-    return SizedBox(
-      width: screenWidth * 0.95,
-      child: Text(
-        'Address:\n$spaddress',
-        textAlign: TextAlign.center,
-        softWrap: true,
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        actions: [
-          TextButton(
-            onPressed: _resetWallet,
-            child: const Text(
-              'Reset wallet',
-              style: TextStyle(),
-            ),
+        title: const Text('Silent payments'),
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
         ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () async {
-                _getPeerCount();
-                _updateWalletInfo();
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-              ),
-              child: const Text('Update peer count'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text('Nakamoto peer count: $peercount'),
-            Expanded(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Text(
-                      'Amount: $amount',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    showScanText(),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Visibility(
-                        visible: progress != 0.0,
-                        child: CircularProgressIndicator(
-                          value: progress,
-                          strokeWidth: 6.0,
-                        ),
-                      ),
-                    ),
-                  ]),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            ElevatedButton(
-              onPressed: peercount == 0
-                  ? null
-                  : () async {
-                      await _scanToTip();
-                    },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-              ),
-              child: const Text('scan to tip'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // Text('Scan height: $scanheight'),
-            // Text('Chain height: $tipheight'),
-            // getAddress(),
-            const SizedBox(
-              height: 40,
-            ),
-          ],
-        ),
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: _onItemTapped,
       ),
     );
   }
---
+++ lib/wallet.dart
@@ -0,0 +1,227 @@
+import 'dart:io';
+
+import 'package:donationwallet/ffi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+class WalletScreen extends StatefulWidget {
+  const WalletScreen({super.key});
+
+  @override
+  State<WalletScreen> createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+  int amount = 0;
+  int tipheight = 0;
+  int scanheight = 0;
+  int peercount = 0;
+
+  String spaddress = '';
+
+  double progress = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _setup();
+  }
+
+  Future<void> _setup() async {
+    api.createLogStream().listen((event) {
+      print('RUST: ${event.msg}');
+    });
+
+    api.createScanProgressStream().listen(((event) {
+      int start = event.start;
+      int current = event.current;
+      int end = event.end;
+      double scanned = (current - start).toDouble();
+      double total = (end - start).toDouble();
+      double progress = scanned / total;
+      if (current == end) {
+        progress = 0.0;
+      }
+      setState(() {
+        this.progress = progress;
+        scanheight = current;
+      });
+    }));
+
+    api.createAmountStream().listen((event) {
+      setState(() {
+        amount = event;
+      });
+    });
+
+    final Directory appDocumentsDir = await getApplicationSupportDirectory();
+
+    // this sets up everything except nakamoto
+    await api.setup(filesDir: appDocumentsDir.path);
+
+    final amt = await api.getAmount();
+    setState(() {
+      amount = amt;
+    });
+
+    // this starts nakamoto, will block forever
+    api.startNakamoto(filesDir: appDocumentsDir.path);
+  }
+
+  void _updateWalletInfo() async {
+    print('getting wallet info');
+    final info = await api.getWalletInfo();
+    final spaddress = await api.getReceivingAddress();
+
+    setState(() {
+      scanheight = info.scanHeight;
+      tipheight = info.blockTip;
+      amount = info.amount;
+      this.spaddress = spaddress;
+    });
+
+    print('scan height ${info.scanHeight}');
+    print('block height ${info.blockTip}');
+  }
+
+  Future<void> _scanBlocks(int amount) async {
+    await api.scanNextNBlocks(n: amount);
+  }
+
+  Future<void> _resetWallet() async {
+    await api.resetWallet();
+    SystemNavigator.pop();
+  }
+
+  Future<void> _scanToTip() async {
+    await api.scanToTip();
+  }
+
+  void _getPeerCount() async {
+    final peercount = await api.getPeerCount();
+
+    setState(() {
+      this.peercount = peercount;
+    });
+  }
+
+  Widget showScanText() {
+    final toScan = tipheight - scanheight;
+
+    if (scanheight == 0) {
+      return Text('Press \'update peer count\'',
+          style: Theme.of(context).textTheme.headlineSmall);
+    } else if (toScan == 0) {
+      return Text('Up to date!',
+          style: Theme.of(context).textTheme.headlineSmall);
+    } else {
+      return Text(
+        'New blocks: $toScan',
+        style: Theme.of(context).textTheme.headlineSmall,
+      );
+    }
+  }
+
+  Widget getAddress() {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return SizedBox(
+      width: screenWidth * 0.95,
+      child: Text(
+        'Address:\n$spaddress',
+        textAlign: TextAlign.center,
+        softWrap: true,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    //     title: Text(widget.title),
+    //     actions: [
+    //       TextButton(
+    //         onPressed: _resetWallet,
+    //         child: const Text(
+    //           'Reset wallet',
+    //           style: TextStyle(),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () async {
+              _getPeerCount();
+              _updateWalletInfo();
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(double.infinity, 50),
+            ),
+            child: const Text('Update peer count'),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text('Nakamoto peer count: $peercount'),
+          Expanded(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                'Amount: $amount',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              showScanText(),
+              const SizedBox(
+                height: 20.0,
+              ),
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Visibility(
+                  visible: progress != 0.0,
+                  child: CircularProgressIndicator(
+                    value: progress,
+                    strokeWidth: 6.0,
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          ElevatedButton(
+            onPressed: peercount == 0
+                ? null
+                : () async {
+                    await _scanToTip();
+                  },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(double.infinity, 50),
+            ),
+            child: const Text('scan to tip'),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          // Text('Scan height: $scanheight'),
+          // Text('Chain height: $tipheight'),
+          // getAddress(),
+          const SizedBox(
+            height: 40,
+          ),
+        ],
+      ),
+    );
+    // );
+  }
+}
