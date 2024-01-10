57daf52973e29e7e8694cf883e34425a23f8d071
cygnet <cygnet3@proton.me>
2024-01-10 13:50:07 +0100

    Rename rust library to sp_backend

--- lib/ffi.dart
+++ lib/ffi.dart
@@ -10,12 +10,12 @@ export 'bridge_definitions.dart';
 export 'bridge_generated.dart';
 import 'dart:io' as io;
 
-const _base = 'rust';
+const _base = 'sp_backend';
 
 // On MacOS, the dynamic library is not bundled with the binary,
 // but rather directly **linked** against the binary.
 final _dylib = io.Platform.isWindows ? '$_base.dll' : 'lib$_base.so';
 
-final api = RustImpl(io.Platform.isIOS || io.Platform.isMacOS
+final api = SpBackendImpl(io.Platform.isIOS || io.Platform.isMacOS
     ? DynamicLibrary.executable()
     : DynamicLibrary.open(_dylib));
