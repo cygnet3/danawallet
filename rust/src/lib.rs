4183f45afacc42cfe0b119fc611d82aafde400ea
cygnet <cygnet3@proton.me>
2024-05-28 00:20:04 +0200

    Remove unused db.rs file

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod api;
-mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
-mod logger;
-// mod db;
 mod blindbit;
+mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod constants;
+mod logger;
 mod stream;
