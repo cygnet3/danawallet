7f60408174d4e45c57203708035b9e6336a1369a
cygnet <cygnet3@proton.me>
2024-06-02 16:40:06 +0200

    Add null check for popup functions

--- lib/global_functions.dart
+++ lib/global_functions.dart
@@ -4,25 +4,29 @@ final globalNavigatorKey = GlobalKey<NavigatorState>();
 
 void displayNotification(String text) {
   print(text);
-  final snackBar = SnackBar(
-    content: Text(text),
-  );
-  ScaffoldMessenger.of(globalNavigatorKey.currentContext!)
-      .showSnackBar(snackBar);
+  if (globalNavigatorKey.currentContext != null) {
+    final snackBar = SnackBar(
+      content: Text(text),
+    );
+    ScaffoldMessenger.of(globalNavigatorKey.currentContext!)
+        .showSnackBar(snackBar);
+  }
 }
 
 void showAlertDialog(String title, String text) {
-  showDialog(
-    context: globalNavigatorKey.currentContext!,
-    builder: (BuildContext context) => AlertDialog(
-      title: Text(title),
-      content: SelectableText(text),
-      actions: [
-        TextButton(
-          child: const Text('Close'),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ],
-    ),
-  );
+  if (globalNavigatorKey.currentContext != null) {
+    showDialog(
+      context: globalNavigatorKey.currentContext!,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(title),
+        content: SelectableText(text),
+        actions: [
+          TextButton(
+            child: const Text('Close'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
+  }
 }
