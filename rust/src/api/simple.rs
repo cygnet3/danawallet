8e9c30930476b440aeac3dd483709b160fb352ff
cygnet <cygnet3@proton.me>
2024-09-11 00:49:58 +0200

    Disable frb provided default logger

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,5 +1,7 @@
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
-    // Default utilities - feel free to customize
-    flutter_rust_bridge::setup_default_user_utils();
+    // This function can be used to set up the frb provided logger and stacktrace.
+    // Since we use our own logger, we disable the default one.
+
+    // flutter_rust_bridge::setup_default_user_utils();
 }
