2022ede403e075ff50e61fee4a5cffd112a964d3
cygnet <cygnet3@proton.me>
2024-10-05 12:15:50 +0200

    Move all spending logic to SpendState

--- lib/screens/home/wallet/spend/spend.dart
+++ lib/screens/home/wallet/spend/spend.dart
@@ -1,10 +1,7 @@
-import 'package:danawallet/constants.dart';
 import 'package:danawallet/global_functions.dart';
 import 'package:danawallet/screens/home/wallet/spend/outputs.dart';
 import 'package:danawallet/screens/home/wallet/spend/destination.dart';
-import 'package:danawallet/generated/rust/api/psbt.dart';
 import 'package:danawallet/generated/rust/api/structs.dart';
-import 'package:danawallet/generated/rust/api/wallet.dart';
 import 'package:danawallet/states/spend_state.dart';
 import 'package:danawallet/states/wallet_state.dart';
 import 'package:flutter/material.dart';
@@ -87,89 +84,6 @@ class SpendScreenState extends State<SpendScreen> {
   bool _isSending = false;
   String? _error;
 
-  (String, BigInt?) _newTransactionWithFees(
-      String wallet,
-      Map<String, OwnedOutput> selectedOutputs,
-      List<Recipient> recipients,
-      int feeRate) {
-    try {
-      final (psbt, changeIdx) = createNewPsbt(
-          encodedWallet: wallet,
-          inputs: selectedOutputs,
-          recipients: recipients);
-
-      // todo: use change address for fees instead of first address
-      final psbtWithFee = addFeeForFeeRate(
-          psbt: psbt, feeRate: feeRate, payer: recipients[0].address);
-
-      // get change amount after reducing fees
-      BigInt? changeAmt;
-      if (changeIdx != null) {
-        changeAmt = readAmtFromPsbtOutput(psbt: psbt, idx: changeIdx);
-      }
-
-      final psbtWithSpOutputsFilled =
-          fillSpOutputs(encodedWallet: wallet, psbt: psbtWithFee);
-      return (psbtWithSpOutputsFilled, changeAmt);
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  String _signPsbt(
-    String wallet,
-    String unsignedPsbt,
-  ) {
-    return signPsbt(encodedWallet: wallet, psbt: unsignedPsbt, finalize: true);
-  }
-
-  Future<String> _broadcastSignedPsbt(
-      String signedPsbt, Network network) async {
-    try {
-      final tx = extractTxFromPsbt(psbt: signedPsbt);
-      final txid = await broadcastTx(tx: tx, network: network.toBitcoinNetwork);
-      return txid;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  String _markAsSpent(
-    String wallet,
-    String txid,
-    Map<String, OwnedOutput> selectedOutputs,
-  ) {
-    try {
-      final updatedWallet = markOutpointsSpent(
-          encodedWallet: wallet,
-          spentBy: txid,
-          spent: selectedOutputs.keys.toList());
-      return updatedWallet;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  String _addTxToHistory(
-      String wallet,
-      String txid,
-      List<String> selectedOutpoints,
-      List<Recipient> recipients,
-      BigInt? changeAmount) {
-    final changeAmt = Amount(field0: changeAmount ?? BigInt.from(0));
-    try {
-      final updatedWallet = addOutgoingTxToHistory(
-          encodedWallet: wallet,
-          txid: txid,
-          spentOutpoints: selectedOutpoints,
-          recipients: recipients,
-          change: changeAmt);
-      return updatedWallet;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
   Future<void> onSpendButtonPressed(
       WalletState walletState, SpendState spendState) async {
     {
@@ -187,35 +101,13 @@ class SpendScreenState extends State<SpendScreen> {
         return;
       }
       try {
-        final wallet = await walletState.getWalletFromSecureStorage();
-        final (unsignedPsbt, changeAmt) = _newTransactionWithFees(
-            wallet, spendState.selectedOutputs, spendState.recipients, fees);
-
-        final signedPsbt = _signPsbt(wallet, unsignedPsbt);
-        final sentTxId =
-            await _broadcastSignedPsbt(signedPsbt, walletState.network);
-        final markedAsSpentWallet =
-            _markAsSpent(wallet, sentTxId, spendState.selectedOutputs);
-        final updatedWallet = _addTxToHistory(
-            markedAsSpentWallet,
-            sentTxId,
-            spendState.selectedOutputs.keys.toList(),
-            spendState.recipients,
-            changeAmt);
-
-        // Clear selections
-        spendState.selectedOutputs.clear();
-        spendState.recipients.clear();
-
-        // save the updated wallet
-        walletState.saveWalletToSecureStorage(updatedWallet);
-        await walletState.updateWalletStatus();
+        final txid = await spendState.createSpendTx(walletState, fees);
 
         if (mounted) {
           // navigate to main screen
           Navigator.popUntil(context, (route) => route.isFirst);
 
-          showAlertDialog('Transaction successfully sent', sentTxId);
+          showAlertDialog('Transaction successfully sent', txid);
         }
       } catch (e) {
         setState(() {
