58e1a4c0a7971222538c29e29ec6698aaa7f8b4f
Sosthene <contact@sosthene.net>
2024-10-23 13:12:25 +0200

    Remove manual coin selection and replace it with broken algorithm

--- lib/screens/home/wallet/spend/spend.dart
+++ lib/screens/home/wallet/spend/spend.dart
@@ -1,8 +1,6 @@
 import 'package:bitcoin_ui/bitcoin_ui.dart';
 import 'package:danawallet/data/models/recommended_fee_model.dart';
 import 'package:danawallet/global_functions.dart';
-import 'package:danawallet/screens/home/wallet/spend/outputs.dart';
-import 'package:danawallet/screens/home/wallet/spend/summary_widget.dart';
 import 'package:danawallet/repositories/mempool_api_repository.dart';
 import 'package:danawallet/states/spend_state.dart';
 import 'package:danawallet/states/wallet_state.dart';
@@ -141,8 +139,6 @@ class SpendScreenState extends State<SpendScreen> {
     final spendState = Provider.of<SpendState>(context, listen: true);
     final walletState = Provider.of<WalletState>(context, listen: false);
 
-    final selectedOutputs = spendState.selectedOutputs;
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false, // removes back btn
@@ -206,17 +202,6 @@ class SpendScreenState extends State<SpendScreen> {
                 });
               },
             ),
-            const Spacer(),
-            SummaryWidget(
-                displayText: selectedOutputs.isEmpty
-                    ? "Tap here to choose which coin to spend"
-                    : "Spending ${selectedOutputs.length} output(s) for a total of ${spendState.outputSelectionTotalAmt()} sats available",
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => const OutputsScreen()),
-                  );
-                }),
             const Spacer(),
             BitcoinButtonFilled(
               cornerRadius: 10,
