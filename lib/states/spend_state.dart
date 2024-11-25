2506548c9a36febd5a7016bd63faa454c2960e14
Sosthene <contact@sosthene.net>
2024-11-25 12:17:15 +0100

    Update createSpendTx for new select_outputs api

--- lib/states/spend_state.dart
+++ lib/states/spend_state.dart
@@ -59,30 +59,28 @@ class SpendState extends ChangeNotifier {
   }
 
   Future<String> createSpendTx(WalletState walletState, int fees) async {
-    Map<String, ApiOwnedOutput> selectedOutputs = {};
     final wallet = await walletState.getWalletFromSecureStorage();
 
-    final totalOutputsAmt = recipientTotalAmt();
-    final spendableOutputs = walletState.getSpendableOutputs();
-    final target = totalOutputsAmt + BigInt.from(2500); // We just guess that fees won't exceed that, but that's horrible
-    var totalAmount = BigInt.zero;
-    spendableOutputs.forEach((outpoint, output) {
-      if (totalAmount >= target) {
-        return;
-      }
-      totalAmount += output.amount.field0;
-      selectedOutputs[outpoint] = output;
-    });
+    final selectOutputsRes = selectOutputs(ownedOutputs: walletState.ownedOutputs, recipients: recipients, feerate: fees);
 
-    final (unsignedPsbt, changeAmt) =
-        _newTransactionWithFees(wallet, selectedOutputs, recipients, fees);
+    final selectedOutputs = selectOutputsRes.selectedOutputs;
+    final changeValue = selectOutputsRes.changeValue;
+
+    if (changeValue > BigInt.zero) {
+      // We need to add a change output
+      final changeAddress = getWalletInfo(encodedWallet: wallet).changeAddress;
+      recipients.add(ApiRecipient(address: changeAddress, amount: Amount(field0: changeValue), nbOutputs: 1));
+    }
+
+    final unsignedPsbt =
+        _newTransactionWithFees(wallet, selectedOutputs, recipients);
 
     final signedPsbt = _signPsbt(wallet, unsignedPsbt);
     final sentTxId =
         await _broadcastSignedPsbt(signedPsbt, walletState.network);
     final markedAsSpentWallet = _markAsSpent(wallet, sentTxId, selectedOutputs);
     final updatedWallet = _addTxToHistory(markedAsSpentWallet, sentTxId,
-        selectedOutputs.keys.toList(), recipients, changeAmt);
+        selectedOutputs.keys.toList(), recipients, changeValue);
 
     // Clear selections
     recipients.clear();
@@ -94,30 +92,19 @@ class SpendState extends ChangeNotifier {
     return sentTxId;
   }
 
-  (String, BigInt?) _newTransactionWithFees(
+  String _newTransactionWithFees(
       String wallet,
       Map<String, ApiOwnedOutput> selectedOutputs,
-      List<ApiRecipient> recipients,
-      int feeRate) {
+      List<ApiRecipient> recipients) {
     try {
       final (psbt, changeIdx) = createNewPsbt(
           encodedWallet: wallet,
           inputs: selectedOutputs,
           recipients: recipients);
 
-      // todo: use change address for fees instead of first address
-      final psbtWithFee = addFeeForFeeRate(
-          psbt: psbt, feeRate: feeRate, payer: recipients[0].address);
-
-      // get change amount after reducing fees
-      BigInt? changeAmt;
-      if (changeIdx != null) {
-        changeAmt = readAmtFromPsbtOutput(psbt: psbt, idx: changeIdx);
-      }
-
       final psbtWithSpOutputsFilled =
-          fillSpOutputs(encodedWallet: wallet, psbt: psbtWithFee);
-      return (psbtWithSpOutputsFilled, changeAmt);
+          fillSpOutputs(encodedWallet: wallet, psbt: psbt);
+      return psbtWithSpOutputsFilled;
     } catch (e) {
       rethrow;
     }
