0ac00db7dd72f09f9e6da8e6fae9c27660fbe7fa
cygnet <cygnet3@proton.me>
2025-03-25 20:20:04 +0100

    Replace old tx_history screen with contacts screen

---
+++ lib/screens/home/contacts/contacts.dart
@@ -0,0 +1,15 @@
+import 'package:bitcoin_ui/bitcoin_ui.dart';
+import 'package:flutter/material.dart';
+
+class ContactsScreen extends StatelessWidget {
+  const ContactsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      'Coming soon!',
+      style: BitcoinTextStyle.body1(Bitcoin.neutral8),
+    ));
+  }
+}
