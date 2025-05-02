f2990def30f535a0f3e3d32f46933b9eb9f90bd4
Sosthene <contact@sosthene.net>
2025-05-02 14:19:49 +0200

    Add path package + sqflite database boilerplate

---
+++ lib/repositories/database_helper.dart
@@ -0,0 +1,36 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('contacts.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const textNullableType = 'TEXT';
+
+    await db.execute('''
+    CREATE TABLE contacts (
+      id $idType,
+      nym $textType,
+      addresses $textType,
+      imagePath $textNullableType
+    )
+    ''');
+  }
+}
