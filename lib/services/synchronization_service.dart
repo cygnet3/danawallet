90b6b13e98d94605688323b7018ce1a9b7c2859b
Sosthene <contact@sosthene.net>
2025-05-08 22:27:18 +0200

    Automatically sync the wallet in the scheduled sync task

--- lib/services/synchronization_service.dart
+++ lib/services/synchronization_service.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'package:danawallet/global_functions.dart';
 import 'package:danawallet/states/chain_state.dart';
+import 'package:danawallet/states/scan_progress_notifier.dart';
+import 'package:danawallet/states/wallet_state.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 class SynchronizationService {
@@ -25,7 +28,25 @@ class SynchronizationService {
   Future<void> performSynchronizationTask() async {
     try {
       final chainState = Provider.of<ChainState>(context, listen: false);
+      final walletState = Provider.of<WalletState>(context, listen: false);
+      final scanProgress =
+          Provider.of<ScanProgressNotifier>(context, listen: false);
+      
       await chainState.updateChainTip();
+
+      if (chainState.tip == walletState.lastScan) {
+        return;
+      } else if (chainState.tip < walletState.lastScan) {
+        // not sure what we should do here, that's really bad
+        Logger().e('Current height is less than wallet last scan');
+        return;
+      }
+
+      if (scanProgress.scanning) {
+        await scanProgress.interruptScan();
+      } else {
+        await scanProgress.scan(walletState);
+      }
     } catch (e) {
       displayNotification(exceptionToString(e));
     }
