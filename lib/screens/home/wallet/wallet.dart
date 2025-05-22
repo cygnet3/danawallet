99fad8b8514d86252d7ce1806044da5b0a43f001
cygnet <cygnet3@proton.me>
2025-05-23 01:52:52 +0200

    [refactor] Delete unused function

--- lib/screens/home/wallet/wallet.dart
+++ lib/screens/home/wallet/wallet.dart
@@ -8,7 +8,6 @@ import 'package:danawallet/states/wallet_state.dart';
 import 'package:danawallet/widgets/receive_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:barcode_widget/barcode_widget.dart';
 
 class WalletScreen extends StatefulWidget {
   const WalletScreen({super.key});
@@ -19,35 +18,6 @@ class WalletScreen extends StatefulWidget {
 
 class WalletScreenState extends State<WalletScreen> {
   bool hideAmount = false;
-
-  void _showReceiveDialog(BuildContext context, String address) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Your address'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SingleChildScrollView(
-                child: BarcodeWidget(data: address, barcode: Barcode.qrCode()),
-              ),
-              const SizedBox(height: 20),
-              SelectableText(address),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   Widget buildScanProgress(double scanProgress) {
     return Row(
