ac5616927c7fba6ed1c9bfab120df5a3cb80d772
cygnet <cygnet3@proton.me>
2025-05-22 23:54:17 +0200

    Add optional prefixImage to footer button

--- lib/widgets/buttons/footer/footer_button.dart
+++ lib/widgets/buttons/footer/footer_button.dart
@@ -3,22 +3,34 @@ import 'package:flutter/material.dart';
 
 class FooterButton extends StatelessWidget {
   final String title;
+  final AssetImage? prefixImage;
   final VoidCallback? onPressed;
   final bool isLoading;
 
-  const FooterButton(
-      {super.key,
-      required this.title,
-      required this.onPressed,
-      this.isLoading = false});
+  const FooterButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    this.prefixImage,
+    this.isLoading = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return BitcoinButtonFilled(
-      body: Text(
-        title,
-        style: BitcoinTextStyle.body2(Bitcoin.neutral1),
-      ),
+      body: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        if (prefixImage != null)
+          Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: Image(
+                image: prefixImage!,
+                color: Bitcoin.white,
+              )),
+        Text(
+          title,
+          style: BitcoinTextStyle.body2(Bitcoin.white),
+        ),
+      ]),
       onPressed: onPressed,
       cornerRadius: 5.0,
       tintColor: const Color.fromARGB(255, 10, 109, 214),
