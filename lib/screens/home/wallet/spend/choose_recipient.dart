caea7b50848762a3e310b790fcc51083a578f762
Sosthene <contact@sosthene.net>
2025-05-27 00:48:03 +0200

    Add contact logic in choose_recipient

--- lib/screens/home/wallet/spend/choose_recipient.dart
+++ lib/screens/home/wallet/spend/choose_recipient.dart
@@ -1,8 +1,12 @@
 import 'package:bitcoin_ui/bitcoin_ui.dart';
+import 'package:danawallet/data/models/payment_address.dart';
 import 'package:danawallet/data/models/recipient_form.dart';
 import 'package:danawallet/exceptions.dart';
+import 'package:danawallet/generated/rust/api/structs.dart';
 import 'package:danawallet/generated/rust/api/validate.dart';
 import 'package:danawallet/global_functions.dart';
+import 'package:danawallet/repositories/contact_dao.dart';
+import 'package:danawallet/screens/home/contacts/contacts.dart';
 import 'package:danawallet/screens/home/wallet/spend/amount_selection.dart';
 import 'package:danawallet/screens/home/wallet/spend/spend_skeleton.dart';
 import 'package:danawallet/widgets/buttons/footer/footer_button.dart';
@@ -11,9 +15,12 @@ import 'package:danawallet/widgets/qr_code_scanner_widget.dart';
 import 'package:dart_bip353/dart_bip353.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
 
 class ChooseRecipientScreen extends StatefulWidget {
-  const ChooseRecipientScreen({super.key});
+  final String? initialAddress;
+  const ChooseRecipientScreen({super.key, this.initialAddress});
 
   @override
   ChooseRecipientScreenState createState() => ChooseRecipientScreenState();
@@ -22,6 +29,21 @@ class ChooseRecipientScreen extends StatefulWidget {
 class ChooseRecipientScreenState extends State<ChooseRecipientScreen> {
   final TextEditingController addressController = TextEditingController();
   String? _addressErrorText;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.initialAddress != null && widget.initialAddress!.isNotEmpty) {
+      addressController.text = widget.initialAddress!;
+      onContinue();
+    }
+  }
+
+  @override
+  void dispose() {
+    addressController.dispose();
+    super.dispose();
+  }
 
   Future<void> onContinue() async {
     RecipientForm form = RecipientForm();
@@ -54,6 +76,32 @@ class ChooseRecipientScreenState extends State<ChooseRecipientScreen> {
 
       form.recipientAddress = address;
 
+      // Check if we have a contact associated to that address
+      try {
+        form.spAddress = PaymentAddress(
+            ApiSilentPaymentAddress.fromStringRepresentation(
+                address: form.recipientAddress!));
+      } catch (e) {
+        // This is a regular, disposable address, we just continue
+        if (mounted) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const AmountSelectionScreen()));
+        }
+      }
+
+      if (mounted) {
+        final contactDao = Provider.of<ContactDAO>(context, listen: false);
+        final existingContact =
+            await contactDao.addressExistsIn(form.spAddress!);
+
+        if (existingContact != null) {
+          // We already know about that address, keep the contact
+          form.contact = existingContact;
+        }
+      }
+
       if (mounted) {
         Navigator.push(
             context,
@@ -64,6 +112,19 @@ class ChooseRecipientScreenState extends State<ChooseRecipientScreen> {
       setState(() {
         _addressErrorText = exceptionToString(e);
       });
+    }
+  }
+
+  Future<void> onSelectFromContact() async {
+    final PaymentAddress? chosen =
+        await Navigator.of(context).push<PaymentAddress>(
+      MaterialPageRoute(
+        builder: (_) => const ContactsScreen(pickAddress: true),
+      ),
+    );
+    if (chosen != null) {
+      addressController.text = chosen.inner.stringRepresentation;
+      await onContinue();
     }
   }
 
@@ -128,12 +189,10 @@ class ChooseRecipientScreenState extends State<ChooseRecipientScreen> {
                   const SizedBox(
                     height: 10.0,
                   ),
-                  if (isDevEnv)
-                    FooterButtonOutlined(
-                      title: "Choose from Contacts",
-                      onPressed: () => (),
-                      enabled: false,
-                    ),
+                  FooterButtonOutlined(
+                    title: 'Choose from Contacts',
+                    onPressed: onSelectFromContact,
+                  ),
                   const SizedBox(
                     height: 10.0,
                   ),
