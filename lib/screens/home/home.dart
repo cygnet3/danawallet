6f67b91b01a29d8c82782a570cf894489fe388f8
Sosthene <contact@sosthene.net>
2025-05-27 00:45:02 +0200

    Makes HomeScreen stateful and taking an arg sentAddress

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:bitcoin_ui/bitcoin_ui.dart';
+import 'package:danawallet/data/models/payment_address.dart';
 import 'package:danawallet/states/home_state.dart';
 import 'package:danawallet/screens/home/contacts/contacts.dart';
 import 'package:danawallet/screens/home/wallet/wallet.dart';
@@ -6,14 +7,59 @@ import 'package:danawallet/screens/home/settings/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
-  static const List<Widget> _widgetOptions = [
-    WalletScreen(),
-    ContactsScreen(),
-    SettingsScreen(),
-  ];
+class HomeScreen extends StatefulWidget {
+  /// If non-null, shows a hint to add this address as a contact on load.
+  final PaymentAddress? sentAddress;
 
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key, this.sentAddress}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if (widget.sentAddress != null) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showModalBottomSheet(
+          context: context,
+          builder: (ctx) => SafeArea(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.person_add),
+                  title: const Text('Create new contact'),
+                  onTap: () {
+                    Navigator.pop(ctx);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => CreateContactScreen(
+                              newAddress: widget.sentAddress!)),
+                    );
+                  },
+                ),
+                // ListTile(
+                //   leading: Icon(Icons.person_search),
+                //   title: Text('Add to existing contact'),
+                //   onTap: () {
+                //     Navigator.pop(ctx);
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(builder: (_) => SelectContactScreen(address: widget.sentAddress!)),
+                //     );
+                //   },
+                // ),
+              ],
+            ),
+          ),
+        );
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
