13112f0762a9a1699a0db00216a8bf9000213694
cygnet <cygnet3@proton.me>
2025-06-07 16:37:58 +0200

    Add 'enabled' and 'isLoading' to plain & outlined footer buttons

--- lib/widgets/buttons/footer/footer_button_outlined.dart
+++ lib/widgets/buttons/footer/footer_button_outlined.dart
@@ -4,9 +4,16 @@ import 'package:flutter/material.dart';
 class FooterButtonOutlined extends StatelessWidget {
   final String title;
   final VoidCallback? onPressed;
+  final bool isLoading;
+  final bool enabled;
 
-  const FooterButtonOutlined(
-      {super.key, required this.title, required this.onPressed});
+  const FooterButtonOutlined({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    this.isLoading = false,
+    this.enabled = true,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +23,10 @@ class FooterButtonOutlined extends StatelessWidget {
       onPressed: onPressed,
       cornerRadius: 5.0,
       tintColor: Bitcoin.neutral5,
+      isLoading: isLoading,
+      disabled: !enabled,
+      // note: disabled color doesn't seem to work
+      disabledTintColor: Colors.grey,
     );
   }
 }
