8958aa7ae71c4d51390ae5421719e2f77688e1e9
cygnet <cygnet3@proton.me>
2025-07-13 20:05:24 +0200

    Add seed phrase recovery and show screens

---
+++ lib/widgets/pills/mnemonic_pill.dart
@@ -0,0 +1,48 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:bitcoin_ui/bitcoin_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class MnemonicPill extends StatelessWidget {
+  final int number;
+  final String word;
+
+  const MnemonicPill({super.key, required this.number, required this.word});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+            child: Container(
+          decoration: BoxDecoration(
+            // border: Border.all(color: Bitcoin.black),
+            color: Bitcoin.neutral3,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30),
+              bottomLeft: Radius.circular(30),
+            ),
+          ),
+          child: Center(
+              child: Text(number.toString(),
+                  style: BitcoinTextStyle.title3(Bitcoin.black))),
+        )),
+        SizedBox(width: Adaptive.w(1)),
+        Flexible(
+            flex: 3,
+            child: Container(
+                decoration: BoxDecoration(
+                  // border: Border.all(color: Bitcoin.black),
+                  color: Bitcoin.neutral3,
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                child: Center(
+                  child: AutoSizeText(word,
+                      style: BitcoinTextStyle.title3(Bitcoin.black)),
+                ))),
+      ],
+    );
+  }
+}
