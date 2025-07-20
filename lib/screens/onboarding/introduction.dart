034243e168028174754c24d55d8400024ff754b4
Sosthene <contact@sosthene.net>
2025-07-20 23:02:04 +0200

    Update wording on the splash screen

--- lib/screens/onboarding/introduction.dart
+++ lib/screens/onboarding/introduction.dart
@@ -28,7 +28,7 @@ class IntroductionScreen extends StatelessWidget {
           maxLines: 1,
         ),
         AutoSizeText(
-          'Convenient, private payments',
+          'Send and receive payments without middlemen',
           style: BitcoinTextStyle.body1(Bitcoin.neutral8)
               .copyWith(height: 1.8, fontFamily: 'Inter'),
           maxLines: 1,
