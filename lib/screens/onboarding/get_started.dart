a5fcfaa4db17f70c14201959cbee3d5762b99a4a
cygnet <cygnet3@proton.me>
2025-07-23 16:23:28 +0200

    Move loading bip39 words to before input screen

--- lib/screens/onboarding/get_started.dart
+++ lib/screens/onboarding/get_started.dart
@@ -1,5 +1,4 @@
 import 'package:bitcoin_ui/bitcoin_ui.dart';
-import 'package:danawallet/constants.dart';
 import 'package:danawallet/data/enums/network.dart';
 import 'package:danawallet/exceptions.dart';
 import 'package:danawallet/global_functions.dart';
@@ -15,13 +14,14 @@ import 'package:danawallet/widgets/buttons/footer/footer_button.dart';
 import 'package:danawallet/widgets/buttons/footer/footer_button_outlined.dart';
 import 'package:danawallet/widgets/icons/circular_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 class GetStartedScreen extends StatelessWidget {
   const GetStartedScreen({super.key});
 
-  Future<void> onRestoreWallet(BuildContext context) async {
+  Future<void> onRestoreFile(BuildContext context) async {
     try {
       final walletState = Provider.of<WalletState>(context, listen: false);
       final chainState = Provider.of<ChainState>(context, listen: false);
@@ -92,6 +92,27 @@ class GetStartedScreen extends StatelessWidget {
     }
   }
 
+  Future<void> onRestoreMnemonic(BuildContext context) async {
+    // load bip39 words from asset file
+    final String wordsText = await rootBundle.loadString('assets/english.txt');
+    final bip39Words = wordsText
+        .split('\n')
+        .map((word) => word.trim())
+        .where((word) => word.isNotEmpty)
+        .toList();
+
+    // go to input seed phrase screen
+    if (context.mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => SeedPhraseScreen(
+                  bip39Words: bip39Words,
+                )),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final body = Column(
@@ -123,18 +144,13 @@ class GetStartedScreen extends StatelessWidget {
       children: [
         if (isDevEnv)
           FooterButtonOutlined(
-              title: 'Restore using file backup',
-              onPressed: () => onRestoreWallet(context)),
+              title: 'Restore (file backup)',
+              onPressed: () => onRestoreFile(context)),
         const SizedBox(
           height: 15,
         ),
         FooterButtonOutlined(
-            title: 'Restore',
-            onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SeedPhraseScreen()),
-                )),
+            title: 'Restore', onPressed: () => onRestoreMnemonic(context)),
         const SizedBox(
           height: 15,
         ),
