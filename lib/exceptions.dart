1581392fc8fb1516dafc17c2a72bafba8b5b3e81
cygnet <cygnet3@proton.me>
2025-07-31 22:56:52 +0200

    Set mainnet as default network for live flavor

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -3,3 +3,5 @@ class InvalidAddressException implements Exception {}
 class InvalidNetworkException implements Exception {}
 
 class UninitializedExchangeRateException implements Exception {}
+
+class UnknownFlavorException implements Exception {}
