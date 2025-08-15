c77d4e536b619d678e8cae3f3538c6afae6b65a4
cygnet <cygnet3@proton.me>
2025-08-15 16:01:40 +0200

    Add variable minor units support
    The Japanese yen has no decimals, add support for variable rounding.

--- lib/generated/rust/api/structs.dart
+++ lib/generated/rust/api/structs.dart
@@ -9,6 +9,7 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 import 'package:freezed_annotation/freezed_annotation.dart' hide protected;
 part 'structs.freezed.dart';
 
+// These functions are ignored because they are not marked as `pub`: `minor_units`
 // These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `try_from`
 
 class ApiAmount {
