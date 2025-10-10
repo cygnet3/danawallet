c318b03ad5e2438b2a5d6096402c754f588f9121
Sosthene <contact@sosthene.net>
2025-10-10 23:02:30 +0200

    Actual fee estimation + custom fees screen

--- lib/screens/home/wallet/spend/fee_selection.dart
+++ lib/screens/home/wallet/spend/fee_selection.dart
@@ -2,9 +2,11 @@ import 'package:bitcoin_ui/bitcoin_ui.dart';
 import 'package:danawallet/data/models/recipient_form.dart';
 import 'package:danawallet/data/models/recipient_form_filled.dart';
 import 'package:danawallet/data/enums/selected_fee.dart';
+import 'package:danawallet/generated/rust/api/structs.dart';
 import 'package:danawallet/global_functions.dart';
 import 'package:danawallet/screens/home/wallet/spend/ready_to_send.dart';
 import 'package:danawallet/screens/home/wallet/spend/spend_skeleton.dart';
+import 'package:danawallet/screens/home/wallet/spend/custom_fee_screen.dart';
 import 'package:danawallet/states/fiat_exchange_rate_state.dart';
 import 'package:danawallet/states/wallet_state.dart';
 import 'package:danawallet/widgets/buttons/footer/footer_button.dart';
@@ -20,10 +22,59 @@ class FeeSelectionScreen extends StatefulWidget {
 }
 
 class FeeSelectionScreenState extends State<FeeSelectionScreen> {
-  SelectedFee? _selected = SelectedFee.normal;
+  SelectedFee _selected = SelectedFee.normal;
+  final Map<SelectedFee, ApiAmount> _feeAmounts = {};
+  bool _isLoadingFees = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _computeFeeAmounts();
+  }
+
+  void _computeFeeAmounts() async {
+    final walletState = Provider.of<WalletState>(context, listen: false);
+    RecipientForm form = RecipientForm();
+
+    for (SelectedFee fee in [
+      SelectedFee.fast,
+      SelectedFee.normal,
+      SelectedFee.slow
+    ]) {
+      form.selectedFee = fee;
+      final filled = form.toFilled();
+      final feeEstimationTx =
+          await walletState.createUnsignedTxToThisRecipient(filled);
+      BigInt inputSum = BigInt.from(0);
+      for (var (_, utxo) in feeEstimationTx.selectedUtxos) {
+        inputSum += utxo.amount.field0;
+      }
+      BigInt outputSum = BigInt.from(0);
+      for (var recipient in feeEstimationTx.recipients) {
+        outputSum += recipient.amount.field0;
+      }
+      _feeAmounts[fee] = ApiAmount(field0: inputSum - outputSum);
+    }
+
+    if (mounted) {
+      setState(() {
+        _isLoadingFees = false;
+      });
+    }
+  }
+
+  // Get the fee amount for a specific fee type
+  ApiAmount? getFeeAmount(SelectedFee fee) {
+    return _feeAmounts[fee];
+  }
+
+  // Get the fee amount for the currently selected fee
+  ApiAmount? getCurrentSelectedFeeAmount() {
+    return _feeAmounts[_selected];
+  }
 
   Future<void> onContinue() async {
-    RecipientForm().fee = _selected!;
+    RecipientForm().selectedFee = _selected;
 
     final walletState = Provider.of<WalletState>(context, listen: false);
     final changeAddress = walletState.changeAddress;
@@ -46,12 +97,37 @@ class FeeSelectionScreenState extends State<FeeSelectionScreen> {
   }
 
   ListTile toListTile(SelectedFee fee, FiatExchangeRateState exchangeRate) {
-    final currentFeeRates = RecipientForm().currentFeeRates!;
-    final estimatedFee = fee.getEstimatedFee(currentFeeRates);
     switch (fee) {
       case SelectedFee.fast:
       case SelectedFee.normal:
       case SelectedFee.slow:
+        if (_isLoadingFees) {
+          return ListTile(
+            title: Row(
+              children: [
+                Text(
+                  fee.toName,
+                  style: BitcoinTextStyle.body3(Bitcoin.black),
+                ),
+                const Spacer(),
+                Text(fee.toEstimatedTime,
+                    style: BitcoinTextStyle.body3(Bitcoin.black)),
+              ],
+            ),
+            subtitle: Text('Loading...',
+                style: BitcoinTextStyle.body3(Bitcoin.black)),
+            leading: Radio<SelectedFee>(
+              groupValue: _selected,
+              value: fee,
+              onChanged: null, // Disabled while loading
+            ),
+          );
+        }
+
+        final estimatedFee = _feeAmounts[fee];
+        if (estimatedFee == null) {
+          throw Exception('Fee amount not computed for $fee');
+        }
         return ListTile(
           title: Row(
             children: [
@@ -77,9 +153,11 @@ class FeeSelectionScreenState extends State<FeeSelectionScreen> {
             groupValue: _selected,
             value: fee,
             onChanged: (SelectedFee? value) {
-              setState(() {
-                _selected = value;
-              });
+              if (value != null) {
+                setState(() {
+                  _selected = value;
+                });
+              }
             },
           ),
         );
@@ -89,18 +167,15 @@ class FeeSelectionScreenState extends State<FeeSelectionScreen> {
             SelectedFee.custom.toName,
             style: BitcoinTextStyle.body3(Bitcoin.black),
           ),
-          leading: Radio<SelectedFee>(
-            groupValue: _selected,
-            value: SelectedFee.custom,
-            onChanged: (SelectedFee? value) {
-              setState(() {
-                _selected = value;
-              });
-            },
-          ),
           trailing: const Image(
             image: AssetImage("icons/caret_right.png", package: "bitcoin_ui"),
           ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CustomFeeScreen()),
+            );
+          },
         );
     }
   }
