c22bd2327d7da9fc47490f9bbc7c592f302e8b84
Sosthene <contact@sosthene.net>
2025-11-10 17:43:46 +0100

    Create username and register danaAddress

--- lib/repositories/settings_repository.dart
+++ lib/repositories/settings_repository.dart
@@ -5,6 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 const String _keyBlindbitUrl = "blindbiturl";
 const String _keyDustLimit = "dustlimit";
 const String _keyFiatCurrency = "fiatcurrency";
+const String _keyUserAlias = "useralias";
+const String _keyDanaAddress = "danaaddress";
 
 class SettingsRepository {
   final SharedPreferencesAsync prefs = SharedPreferencesAsync();
@@ -16,8 +18,13 @@ class SettingsRepository {
   static final instance = SettingsRepository._();
 
   Future<void> resetAll() async {
-    await prefs
-        .clear(allowList: {_keyBlindbitUrl, _keyDustLimit, _keyFiatCurrency});
+    await prefs.clear(allowList: {
+      _keyBlindbitUrl,
+      _keyDustLimit,
+      _keyFiatCurrency,
+      _keyUserAlias,
+      _keyDanaAddress
+    });
   }
 
   Future<void> setBlindbitUrl(String? url) async {
@@ -56,6 +63,30 @@ class SettingsRepository {
     final currency = await prefs.getString(_keyFiatCurrency);
 
     return currency != null ? FiatCurrency.values.byName(currency) : null;
+  }
+
+  Future<void> setUserAlias(String alias) async {
+    return await prefs.setString(_keyUserAlias, alias);
+  }
+
+  Future<String?> getUserAlias() async {
+    return await prefs.getString(_keyUserAlias);
+  }
+
+  Future<void> clearUserAlias() async {
+    return await prefs.remove(_keyUserAlias);
+  }
+
+  Future<void> setDanaAddress(String danaAddress) async {
+    return await prefs.setString(_keyDanaAddress, danaAddress);
+  }
+
+  Future<String?> getDanaAddress() async {
+    return await prefs.getString(_keyDanaAddress);
+  }
+
+  Future<void> clearDanaAddress() async {
+    return await prefs.remove(_keyDanaAddress);
   }
 
   Future<SettingsBackup> createSettingsBackup() async {
