c065380b5e02ff660978de90a2fd11ef065988ed
Sosthene <contact@sosthene.net>
2025-11-28 23:52:25 +0100

    Add contacts data model

---
+++ lib/data/models/contacts.dart
@@ -0,0 +1,31 @@
+class Contact {
+  int? id;
+  final String nym; // user defined user name
+  final String danaAddress; // Eventually register more than one, for now keep it simple
+  final String spAddress; // silent payment address of the contact
+
+  Contact({
+    this.id,
+    required this.nym,
+    required this.danaAddress,
+    required this.spAddress,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nym': nym,
+      'danaAddress': danaAddress,
+      'spAddress': spAddress,
+    };
+  }
+
+  factory Contact.fromMap(Map<String, dynamic> map) {
+    return Contact(
+      id: map['id'],
+      nym: map['nym'],
+      danaAddress: map['danaAddress'],
+      spAddress: map['spAddress'],
+    );
+  }
+}
