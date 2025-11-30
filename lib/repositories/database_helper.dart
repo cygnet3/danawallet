ca0c72bc6e89eb1eb56d5db99aadf711f442b36c
Sosthene <contact@sosthene.net>
2025-11-30 23:25:45 +0100

    Add contact_field to db (upgrade to version 2)

--- lib/repositories/database_helper.dart
+++ lib/repositories/database_helper.dart
@@ -18,9 +18,12 @@ class DatabaseHelper {
     final path = join(dbPath, filePath);
     return await openDatabase(
       path,
-      version: 1,
+      version: 2,
       onCreate: (db, version) async {
         await _createDB(db, version);
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        await _upgradeDB(db, oldVersion, newVersion);
       },
     );
   }
@@ -29,6 +32,7 @@ class DatabaseHelper {
     const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
     const textType = 'TEXT NOT NULL';
     const textTypeNullable = 'TEXT';
+    const intType = 'INTEGER NOT NULL';
 
     await db.execute('''
     CREATE TABLE contacts (
@@ -38,6 +42,45 @@ class DatabaseHelper {
       spAddress $textType
     )
     ''');
+
+    await db.execute('''
+    CREATE TABLE contact_fields (
+      id $idType,
+      contact_id $intType,
+      field_type $textType,
+      field_value $textType,
+      FOREIGN KEY (contact_id) REFERENCES contacts(id) ON DELETE CASCADE
+    )
+    ''');
+
+    // Create index for faster queries
+    await db.execute('''
+    CREATE INDEX idx_contact_fields_contact_id ON contact_fields(contact_id)
+    ''');
+  }
+
+  Future _upgradeDB(Database db, int oldVersion, int newVersion) async {
+    if (oldVersion < 2) {
+      const intType = 'INTEGER NOT NULL';
+      const textType = 'TEXT NOT NULL';
+      const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+
+      // Create contact_fields table
+      await db.execute('''
+      CREATE TABLE contact_fields (
+        id $idType,
+        contact_id $intType,
+        field_type $textType,
+        field_value $textType,
+        FOREIGN KEY (contact_id) REFERENCES contacts(id) ON DELETE CASCADE
+      )
+      ''');
+
+      // Create index for faster queries
+      await db.execute('''
+      CREATE INDEX idx_contact_fields_contact_id ON contact_fields(contact_id)
+      ''');
+    }
   }
 
   Future<void> close() async {
