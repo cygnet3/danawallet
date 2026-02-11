145f5fee6703c863079f4aade8de44da12cc42c5
cygnet <cygnet3@proton.me>
2026-02-11 18:19:23 +0100

    refactor(rust): rename blindbit-backend -> backend-blindbit-v1

--- rust/src/api/chain.rs
+++ rust/src/api/chain.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
+use backend_blindbit_v1::{BlindbitBackend, BlindbitClient};
 use log::warn;
 use spdk_core::{bitcoin::Network, ChainBackend};
-use blindbit_backend::{BlindbitBackend, BlindbitClient};
 use tokio::time::sleep;
 
 pub async fn get_chain_height(blindbit_url: String) -> anyhow::Result<u32> {
